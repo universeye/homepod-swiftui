26df8b55154304bd12497edc41de7b17cebad151
Terry <terrykuo501@gmail.com>
2021-07-06 14:46:15 +0800

    Initial Commit

---
+++ HomePodSwiftUI/HomePodSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePodSwiftUIApp.swift
+//  HomePodSwiftUI
+//
+//  Created by Terry Kuo on 2021/7/6.
+//
+
+import SwiftUI
+
+@main
+struct HomePodSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
