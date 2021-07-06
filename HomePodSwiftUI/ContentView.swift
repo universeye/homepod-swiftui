9f3e93ce97ad3133ea5b77ced28486e967880830
Terry <terrykuo501@gmail.com>
2021-07-06 14:55:22 +0800

    Initial Commit

--- HomePodSwiftUI/ContentView.swift
+++ HomePodSwiftUI/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+//            Circle()
+//                .fill(Color.black)
+            ZStack {
+                ForEach(0..<130) { index in
+                    Circle()
+                        .strokeBorder(Color(white: 0.15), lineWidth: 1.25)
+                        .frame(width: 160, height: 160)
+                        .offset(x: 120)
+                }
+            }
+        }
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
+    
+    struct HomePodTopView_Harness: View {
+        var body: some View {
+            ContentView()
+                .frame(width: 400, height: 400)
+                .padding(50)
+        }
+    }
     static var previews: some View {
-        ContentView()
+        HomePodTopView_Harness()
+            .previewLayout(.sizeThatFits)
     }
 }
